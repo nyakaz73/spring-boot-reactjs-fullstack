50cfac33e718237dbb60ce64b88d46dcb3fe1bb1
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-08 13:44:36 +0200

    Initial Commit

---
+++ src/main/java/com/datsystemz/nyakaz/springbootreactjsfullstack/SpringBootReactJsFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.datsystemz.nyakaz.springbootreactjsfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactJsFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactJsFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datsystemz/nyakaz/springbootreactjsfullstack/SpringBootReactJsFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datsystemz.nyakaz.springbootreactjsfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactJsFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
