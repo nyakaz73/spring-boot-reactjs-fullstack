ac8d95a2f2cfead5d3ba0aecc6a5388647f42c53
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-08 21:10:30 +0200

    Configure webpack.conf,js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/App.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|eot|otf|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    }
+};
