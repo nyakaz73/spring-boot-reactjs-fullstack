c2a7795f38619ee8468304d1d453503563caa471
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-08 17:04:35 +0200

    Created a UserController

---
+++ src/main/java/com/datsystemz/nyakaz/springbootreactjsfullstack/controllers/UserController.java
@@ -0,0 +1,72 @@
+package com.datsystemz.nyakaz.springbootreactjsfullstack.controllers;
+
+import com.datsystemz.nyakaz.springbootreactjsfullstack.exceptions.ResourceNotFoundException;
+import com.datsystemz.nyakaz.springbootreactjsfullstack.models.User;
+import com.datsystemz.nyakaz.springbootreactjsfullstack.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/user/save")
+    public User saveUser(@RequestBody User user){
+        return this.userRepository.save(user);
+    }
+
+    @GetMapping("/user/all")
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok(
+          this.userRepository.findAll()
+        );
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "id" ) Long id){
+        User user = this.userRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("User not found")
+        );
+
+        return  ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("user/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable(value = "id") Long id){
+        return this.userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setSurname(newUser.getSurname());
+                    user.setEmail(newUser.getEmail());
+                    user.setUsername(newUser.getUsername());
+                    user.setPassword(newUser.getPassword());
+                    return this.userRepository.save(user);
+                })
+                .orElseGet(()->{
+                   newUser.setId(id);
+                   return this.userRepository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("user/{id}")
+    public ResponseEntity<Void> removeUser(@PathVariable(value = "id") Long id){
+        User user =this.userRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("User not found"+id)
+        );
+
+        this.userRepository.delete(user);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+}
